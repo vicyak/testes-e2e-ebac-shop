6c26d1b6f40a5608f5d60000eb1f47baa8c3ccc5
vicyak <89476381+vicyak@users.noreply.github.com>
2023-06-24 17:52:18 -0300

    Add files via upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,13 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProdutos',(produto, tamanho, cor, quantidade) =>{
+    cy.get('[class="product-block grid"]')
+            .contains(produto).click()
+            cy.get('.button-variable-item-'+tamanho).click()
+            cy.get('.button-variable-item-'+cor).click()
+            cy.get('.input-text').clear()
+            cy.get('.input-text').type(quantidade)
+            cy.get('.single_add_to_cart_button').click()
+
+ })
