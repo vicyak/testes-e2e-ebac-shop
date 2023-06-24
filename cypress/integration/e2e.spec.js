ff621abf8e5d40422cb6c1263fa4f43b8b1dce26
vicyak <89476381+vicyak@users.noreply.github.com>
2023-06-24 17:51:38 -0300

    Add files via upload

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,5 @@
-/// <reference types="cypress" />
-
+///<reference types="cypress"/>
+const perfil =require ('../fixtures/perfil.json')
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -8,14 +8,47 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Adicionando ao carrinho
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
-
+   
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('')
     });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+    it.only('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        cy.visit('/minha-conta')
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()//faz login usando fixtures
+
+        cy.get('.page-title').should('contain', 'Minha conta')//verifica se o titulo do titulo e corretp
+
+        cy.get('#primary-menu > .menu-item-629 > a').click()//vai para pagina de produtos
+
+        cy.get('.page-title').should('contain', 'Produtos')//valida que foi para pagina de produtos
+
+        cy.addProdutos('Atlas Fitness Tank', 'M', 'Blue', 3)//adc o produto. tamanho e quantidade
+        cy.get('#primary-menu > .menu-item-629 > a').click()//vai para pagina de produtos
+
+        cy.addProdutos('Abominable Hoodie', 'XS', 'Blue', 3)//adc o produto. tamanho e quantidade
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(5) > a').click()//vai para pagina de produtos
+        
+        cy.addProdutos('Oslo Trek Hoodie', 'M', 'Purple', 2)//adc o produto. tamanho e quantidade
+        cy.get('.woocommerce-message > .button').click()// vai para o carrinho
+
+        cy.get('.checkout-button').click()//vai para concluir compra
+        cy.wait(3000)//espera a pagina carregar
+
+
+        cy.get('.woocommerce-terms-and-conditions-checkbox-text').click()//seleciona o checkbox de termos e condicoes
+        cy.get('#place_order').click()// finaliza o pedido
+
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')//verifica se o pedido foi recebido corretamente
+
+
     });
 
 
-})
+        
+    });
+ 
+
+
